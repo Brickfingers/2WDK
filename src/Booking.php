f904161a0dd11e94e7ea2cbf5ebc2a04a4494342
Robert Wright <wrig0410@algonquinlive.com>
2020-03-06 10:17:45 -0500

    Booking

---
+++ src/Booking.php
@@ -0,0 +1,67 @@
+<?php #include "header.php"; ?>
+
+<html>
+<head>
+<title>Booking</title>
+<!-- <link rel="stylesheet" type="text/css" href="Booking.css" />*/-->
+</head>
+<body>
+
+<h1 style="text-align: center; white-space; pre-wrap;"> Booking </h1>
+
+<form method="post">
+<label>How did you hear about Ben Joseph?</label>
+<input type="text"></input><br>
+
+<label>First Name</label>
+<input type="text"></input><br>
+
+<label>Last Name</label>
+<input type="text"></input><br>
+
+<label>Email Address</label>
+<input type="email"></input><br>
+
+<label>Phone Number</label>
+<input type="text" maxlength="3"></input>
+<input type="text" maxlength="3"></input>
+<input type="text" maxlength="3"></input><br>
+
+<label>Website</label>
+<input type="text"></input><br>
+
+<label>Type of Event</label>
+<input type="text"></input><br>
+
+<label>Your event is:</label>
+<input type="checkbox" value ="private"></input>
+<input type="checkbox" value ="public"></input><br>
+
+<label>What is the start and end time of your event?</label>
+<input></input><br>
+
+<label>Where is your event located?</label>
+<input></input><br>
+
+<label>How long would you like Ben Joseph to perform?</label>
+<input></input><br>
+
+<label>What genre of music would you like to be played?</label>
+<input></input><br>
+
+<label>What musical performance type are you seeking?</label>
+<input></input><br>
+
+<label>Does the venue have a P.A. system or in-house sound/audio</label>
+<input></input><br>
+
+<label>Please list any additional information you would like to include about the event</label>
+<textarea></textarea><br>
+</form>
+
+
+
+</body>
+</html>
+
+<?php #include "footer.php"; ?>
