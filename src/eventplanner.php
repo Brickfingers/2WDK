14cd83d29912a0c84c93b0d07b4dfd2c69ec76d4
joshaw807 <47795605+joshaw807@users.noreply.github.com>
2020-03-16 18:31:22 -0400

    calendar test

--- src/eventplanner.php
+++ src/eventplanner.php
@@ -1,13 +1,19 @@
 <html>
 <head>   
+<?php include_once 'header.php';?>
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
 </head>
 <body>
     <div class="container">
-    <div class="page-header"><h1>Events</h1></div>
-    
+    	<div class="page-header"><h1>Events</h1></div>
+  <div class="embed-responsive embed-responsive-16by9">
 
-<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FToronto&amp;src=dGVhbTJ3ZGtAZ21haWwuY29t&amp;color=%23039BE5&amp;showPrint=0&amp;showTitle=0&amp;showTz=0&amp;title=2WDK" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>    </div>
+		<iframe class="embed-responsive-item" src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FToronto&amp;src=dGVhbTJ3ZGtAZ21haWwuY29t&amp;color=%23039BE5&amp;showPrint=0&amp;showTitle=0&amp;showTz=0&amp;title=2WDK" style="border:solid 1px #777" width="800" height="600" ></iframe>    </div>
+	</div>
 </body>
+<footer>
+<?php include_once 'footer.php';?>
+</footer>
 </html>  
