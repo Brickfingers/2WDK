86c613588c8a9ec09eda31da1952ccaa147e78c7
dasb1996 <60516938+dasb1996@users.noreply.github.com>
2020-03-19 11:14:57 -0400

    footer commit

--- src/footer.php
+++ src/footer.php
@@ -1,77 +1,108 @@
-<!doctype html>
+<footer class="page-footer font-small stylish-color-dark pt-4">
+<div class= footer>
+  <div class="container text-center text-md-left">
+	<link rel="stylesheet" href="footer.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <div class="row">
 
-    <link rel="stylesheet" href="footer.css">
+      
+      <div class="col-md-3 mx-auto">     
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">ABOUT US</h5>
+        <p>INSERT TEXT HERE.</p>
+      </div>
+     
+      <hr class="clearfix w-100 d-md-none">
+    
+      <div class="col-md-2 mx-auto">
+       <h5 class="font-weight-bold text-uppercase mt-3 mb-4">MENU</h5>
+        <ul class="list-unstyled">
+           <li>
+            <a href="#!">HOME</a>
+          </li>
+		  <li>
+            <a href="#!">ABOUT</a>
+          </li>
+          <li>
+            <a href="#!">GALLERY</a>
+          </li>
+          <li>
+            <a href="#!">BOOKING</a>
+          </li>
+		   <li>
+            <a href="#!">EVENTS</a>
+          </li>
+        </ul>
+      </div>
 
-    <footer class="footer">
-        <div class="footer_top">
-            <div class="footercontainer">
-                <div class="row">
-                    <div class="column1">
-                        <div class="footer_container ">
-                        <p></p>
-                            <h3 class="footer_title">
-                                About
-                            </h3>
-                            <p>Text here</p>
-                            </div>
-                    </div>
-                   
-                    <div class="column2">
-                        <div class="footer_container ">
-                        <p></p>
-                            <h3 class="footer_title">
-                                Menu
-                            </h3>
-                            <ul>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Gallery</a></li>
-                                <li><a href="#">Calendar</a></li>
-                                <li><a href="#">Booking</a></li>
-                                <li><a href="#">Sign up</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="column3">
-                        <div class="footer_container">
-                        <p></p>
-                            <h3 class="footer_title">
-                                Newsletter
-                            </h3>
-                            <p class="signup">
-                                Enter your email to get notifications.
-                            </p>
-                            <form action="#" class="sigup">
-                                <input type="text" placeholder="Enter your email">
-                                <button type="submit">Sign up!</button>
-                            </form>
-                        </div>
-                    </div>
-                     <div class="column4">
-                        <div class="footer_container ">
-                        <p></p>
-                            <h3 class="footer_title">
-                                Connect
-                                
-                            </h3>
-                            <p><a href="mailto:info@domain.com" title="email">info@domain.com</a></p>
-                            <ul>
-                              
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copy-right_text">
-            <div class="footercontainer">
-                <div class="footer_border"></div>
-                <div class="row">
-                    <p class="copy_right">
-                            <p>
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-  						</p>                
-                </div>
-            </div>
-        </div>
-    </footer>
+      <hr class="clearfix w-100 d-md-none">
 
+      <div class="col-md-2 mx-auto">
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">###</h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!"></a>
+          </li>
+          <li>
+            <a href="#!"></a>
+          </li>
+          <li>
+            <a href="#!"></a>
+          </li>
+          <li>
+            <a href="#!"></a>
+          </li>
+        </ul>
+      </div>
+      <hr class="clearfix w-100 d-md-none">
+      <div class="col-md-2 mx-auto">
+
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">NEWSLETTER</h5>
+        <ul class="list-unstyled">    
+    </li>
+    <li class="list-inline-item">
+      <a href="#!" class="btn btn-danger btn-rounded">Sign up!</a>
+    </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  
+  <ul class="list-unstyled list-inline text-center">
+  <h5 class="mb-1">CONTACT US</h5>
+  <br />
+  <li class="list-inline-item">
+      <a class="btn-floating btn-fb mx-1">
+        <a href=""><i class="fa fa-envelope mr-md-3 mr-5 fa-3x"> </i></a>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-fb mx-1">
+        <a href=""><i class="fa fa-facebook mr-md-3 mr-5 fa-3x"> </i></a>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-tw mx-1">
+        <a href=""><i class="fa fa-instagram mr-md-3 mr-5 fa-3x"> </i></a>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-gplus mx-1">
+        <a href=""><i class="fa fa-youtube mr-md-3 mr-3 fa-3x"> </i></a>
+      </a>
+    </li>
+    
+  </ul>
+  <div class="copyright">
+  <br />
+  <div class="footer-copyright text-center py-3">
+  
+ <h6 class="font-weight-bold text-uppercase ">© 2020 Copyright: Sax n Sip</h6>
+
+  </div>
+  </div>
+  </div>
+</footer>
