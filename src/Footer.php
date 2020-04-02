6d582207306e48bb734e9ed4699c0c0771781214
dasb1996 <60516938+dasb1996@users.noreply.github.com>
2020-04-02 19:34:10 -0400

    mailchimp done

--- src/Footer.php
+++ src/Footer.php
@@ -4,6 +4,10 @@
         <div class="container text-center text-md-left">	
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+        	#mc_embed_signup{background:#fff; clear:left; font:17px Helvetica,Arial,sans-serif; }
+        </style>
         <link rel="stylesheet" href="css/Footer.css">
         <div class="row">
          
@@ -15,20 +19,39 @@
         
                 		<div class="subscribe_box" style="max-width:500px">
             				<div class="popup">
-                          		<div class="w3-center"><br>
+                          		                  	  
+                    	  <!-- Begin Mailchimp Signup Form -->
+                            
+                            <div id="mc_embed_signup">
+                            <form action="https://algonquinlive.us19.list-manage.com/subscribe/post?u=af5fac3ff4d76b39f763dbb7b&amp;id=7c52c75b83" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+                                <div class="w3-center"><br/>
+                            	<h2>Sax 'n' Sip</h2><br/>
+                            	
+                                <div class="mc-field-group">
+                                	<label for="mce-EMAIL">Sign up to receive our emails and don't miss any of our upcoming events!</label>
+                                	<br/>
+                                	<input type="email" value="" name="EMAIL" placeholder="Enter your email" class="required email" id="mce-EMAIL">
+                                </div>
+                            	<div id="mce-responses" class="clear">
+                            		<div class="response" id="mce-error-response" style="display:none"></div>
+                            		<div class="response" id="mce-success-response" style="display:none"></div>
+                            		
+                            	</div>    
+                            	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_af5fac3ff4d76b39f763dbb7b_7c52c75b83" tabindex="-1" value=""></div>
+                                <div class="clear">
+                                <input type="submit" value="Sign Up!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                </div>
+                            </form>
+                            </div>
+                            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                            <!--End mc_embed_signup-->
                     	  
-                    	  			<h2><b>SUBSCRIBE</b></h2>
-                            		<span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Subscribe Popup">&times;</span>
-                          		</div>
-        	
-             				<form class="w3-container" action="/action_page.php">
-               					 <div class="w3-section">
-                                     
-                                      <h4>Please enter your email:</h4><br />
-                                      <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Enter Email" name="email" required>
-                            		  	<div class="w3-center">              		  
-                                       		
-                                        	<a class="button" href="#popup1">Privacy Policy</a>                                                                                    
+                    	  	<span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Subscribe Popup">&times;</span>
+                          		
+        					 </div>
+        					 <div class="w3-center">
+             				<a class="button" href="#popup1">Privacy Policy</a>                                                                                    
                                             <div id="popup1" class="overlay">
                                             	<div class="popup-privacy">
                                             		<a class="close" href="#">&times;</a>
@@ -50,17 +73,15 @@
                                             		</div>
                                             	</div>
                                             </div>
-                            		  </div>
-                                      <button class="w3-button w3-block w3-gray w3-section w3-padding" type="submit"><h5>SEND</h5></button>    
-                				</div>
-              				</form>
+                           	</div>
+                            <br/>               
             			</div>
         			</div>
           		</div>
         	</div>
             </li>
         </div>
-        
+ 
     <div class= footer_no_selected>    
     <div class="footer-copyright text-center py-3">
   		<ul class="list-unstyled list-inline text-center">
@@ -71,19 +92,19 @@
             </li>
           
           	<li class="list-inline-item">
-                <a href="http://www.facebook.com"><i class="fa fa-facebook mr-md-5 mr-5 fa-3x"> </i></a>             
+                <a href="https://www.facebook.com/Sax-N-Sip-with-Ben-Joseph-394791691247339/" target="_blank"><i class="fa fa-facebook mr-md-5 mr-5 fa-3x"> </i></a>             
           	</li>
           
       		<li class="list-inline-item">              
-                <a href="http://www.instagram.co"><i class="fa fa-instagram mr-md-5 mr-5 fa-3x"> </i></a>        
+                <a href="http://www.instagram.com/saxnsip" target="_blank"><i class="fa fa-instagram mr-md-5 mr-5 fa-3x"> </i></a>        
             </li>
          	
          	<li class="list-inline-item">          
-                <a href="http://www.instagram.co"><i class="fa fa-soundcloud mr-md-5 mr-5 fa-3x"> </i></a>         
+                <a href="http://www.instagram.com" target="_blank"><i class="fa fa-soundcloud mr-md-5 mr-5 fa-3x"> </i></a>         
             </li>
            
             <li class="list-inline-item">             
-                <a href="http://www.youtube.com"><i class="fa fa-youtube mr-md-5 mr-5 fa-3x"> </i></a>            
+                <a href="https://www.youtube.com" target="_blank"><i class="fa fa-youtube mr-md-5 mr-5 fa-3x"> </i></a>            
             </li>
       	</ul>
       
