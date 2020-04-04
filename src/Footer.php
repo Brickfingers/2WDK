d2687998f5d3672997cc60b961352fb3e503e5d9
dasb1996 <60516938+dasb1996@users.noreply.github.com>
2020-04-03 22:01:45 -0400

    commit

--- src/Footer.php
+++ src/Footer.php
@@ -13,7 +13,7 @@
          
             <li class="list-inline-item">
             <div class="w3-container">
-        	  <form>
+        	 
           		<button onclick="document.getElementById('id01').style.display='block'" class="open-button"><b>SIGN UP</b></button>
         			<div id="id01" class="w3-modal">
         
@@ -26,7 +26,7 @@
                             <form action="https://algonquinlive.us19.list-manage.com/subscribe/post?u=af5fac3ff4d76b39f763dbb7b&amp;id=7c52c75b83" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                                 <div id="mc_embed_signup_scroll">
                                 <div class="w3-center"><br/>
-                            	<h2>Sax 'n' Sip</h2><br/>
+                            	<h1 class="subscribe_title">Don't miss out!</h1><br/>
                             	
                                 <div class="mc-field-group">
                                 	<label for="mce-EMAIL">Sign up to receive our emails and don't miss any of our upcoming events!</label>
@@ -40,45 +40,46 @@
                             	</div>    
                             	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_af5fac3ff4d76b39f763dbb7b_7c52c75b83" tabindex="-1" value=""></div>
                                 <div class="clear">
-                                <input type="submit" value="Sign Up!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                <input type="submit" value="Sign Up!" name="subscribe" id="mc-embedded-subscribe" class="sign_up_button"></div>
                                 </div>
                             </form>
                             </div>
                             <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
                             <!--End mc_embed_signup-->
                     	  
-                    	  	
+                    	  	<span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Subscribe Popup">&times;</span>
+     
                     	  		
         					 </div>
-        					 <div class="w3-center">
-             				<a class="button" href="#popup1">Privacy Policy</a>                                                                                    
+        					 
+             				<a class="button subscribe_title" href="#popup1">Privacy Policy</a>                                                                                    
                                             <div id="popup1" class="overlay">
                                             	<div class="popup-privacy">
                                             		<a class="close" href="#">&times;</a>
                                             		<div class="content">
                                                 		<div class="policy_text">
-                                                		 <h2>Privacy Policy</h2>
+                                                		<br/>
+                                                		 <h1 class="subscribe_title">Privacy Policy     </h1><br/>
                                                 		<hr>
-                                                			<h3>Subscribe</h3>
+                                                			 <h2 class="subscribe_title">Subscribe</h2>
                                                 			<br/>
                                                 			If you choose to receive emails notifications from Sax 'n' Sip, 
                                                 			we will collect your email address. 
                                                 			<br/><br/>
-                                                			<h3>Booking an event</h3> 
+                                                			 <h2 class="subscribe_title">Booking an event</h2>
                                                 			<br/>
                                                 			You are given the opportunity to book an event with Ben Joseph on our Website. Booking an event requires you to enter your email address, first and last name, 
                                                 			telephone number.
-                                                			<br/><br/>
+                                                			<br/><br/><br/>
                                                 		</div>
                                             		</div>
                                             	</div>
                                             </div>
-                           	</div>
-                            <br/>  
-                            <button type="button" class="close_button" onclick="closeForm()">Close</button><br/>             
+                           	 <br/><br/>
+                                     
             			</div>
         			</div>
-        			</form>
+        			
           		</div>
         	</div>
             </li>
