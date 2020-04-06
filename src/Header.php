8958b11531b4efd1e1b9343a50aa5f276d963cd7
TanyaKolechkina <56099468+TanyaKolechkina@users.noreply.github.com>
2020-04-06 19:14:16 -0400

    new header style

--- src/Header.php
+++ src/Header.php
@@ -7,18 +7,17 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<!-- <link rel="stylesheet" type="text/css" href="css/Header.css" /> -->
 
 <link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 <link href='https://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet'>
-<link rel="stylesheet" type="text/css" href="css2/Header2.css" />
+<link rel="stylesheet" type="text/css" href="css/Header.css" />
+<!-- <link rel="stylesheet" type="text/css" href="css2/Header2.css" /> -->
 <body>	
 	<div class="jumbotron jumbotron-fluid">
       <div class="container text-center">
-        <h1 class="display-2">Sax'n'Sip</h1>
-        <h1 class="display-6">BEN JOSEPH</h1>
-        <br>
+        <h1 class="display-2">&#119070ax n'&#119070ip</h1>             
+        <h1 class="display-6">BEN JOSEPH</h1>        
         <p class="lead">SAXOPHONIST</p>
       </div>
     </div>
