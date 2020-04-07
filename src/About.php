bffed16c0945c7ce45a446db40fb42e66f0a7304
dasb1996 <60516938+dasb1996@users.noreply.github.com>
2020-04-07 15:44:13 -0400

    about page

--- src/About.php
+++ src/About.php
@@ -4,7 +4,7 @@
 <title>Sax N Sip</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css2/About2.css" />
+<link rel="stylesheet" type="text/css" href="css/About.css" />
 <!-- <link rel="stylesheet" type="text/css" href="css2/About2.css" /> -->
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
