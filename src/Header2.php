ec1857501a4dd36a3d1ee582894158ef47e14c22
TanyaKolechkina <56099468+TanyaKolechkina@users.noreply.github.com>
2020-04-08 11:11:12 -0400

    added link to HomePage2.php

--- src/Header2.php
+++ src/Header2.php
@@ -35,7 +35,7 @@
   					<div class="col-8">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="HomePage.php"> HOME </a> 
+                                <a class="nav-link" href="HomePage2.php"> HOME </a> 
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="About2.php"> ABOUT </a>
