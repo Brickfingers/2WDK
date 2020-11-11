2945116af5a9f7b083c0c0fbdac3d08c477b92c0
fahimehKarimi <kari0084@algonquinlive.com>
2020-11-11 13:41:06 -0500

    11/11/2020

--- src/retrieveImage.php
+++ src/retrieveImage.php
@@ -1,7 +1,7 @@
 <?php
 include "DB_Connect.php";
 
-$query = "SELECT * FROM About WHERE AboutId=3";
+$query = "SELECT * FROM About WHERE AboutId=21";
 $result = sqlsrv_query($conn, $query);
 
 if (sqlsrv_fetch($result) === false) {
@@ -12,8 +12,8 @@ $myImage = sqlsrv_get_field($result, 2);
 echo "image name is:  $myImage \n";
 
 /* photo folder that used to store images*/
-
-echo "<img src='photo/" . $myImage . "' height='280' width='220'> ";
+$imagesource= "photo/";
+echo "<img src='$imagesource" . $myImage . "' height='280' width='220'> ";
 
 sqlsrv_free_stmt($result);
 sqlsrv_close($conn);
