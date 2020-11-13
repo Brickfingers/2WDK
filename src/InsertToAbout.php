aba829dd8e1f3a6e29ea374a78ca7d8baa735d59
FahimehKarimi <60516983+FahimehKarimi@users.noreply.github.com>
2020-11-12 20:20:49 -0500

    Update InsertToAbout.php
    nov12

--- src/InsertToAbout.php
+++ src/InsertToAbout.php
@@ -3,58 +3,64 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style type="text/css">
+.next {
+	background-color: #4CAF50;
+	color: white;
+}
+
+a {
+	text-decoration: none;
+	display: inline-block;
+	padding: 8px 16px;
+}
+</style>
+</head>
 <?php
 include "DB_Connect.php";
-if (isset($_POST['upload']) && isset($_POST["about"]) && isset($_POST["title"])) {
+if (isset($_POST['submit'])) {
     $aboutme = $_POST["about"];
     $aboutTitle = $_POST["title"];
     $filename = $_FILES["fileToUpload"]["name"];
     $tempname = $_FILES["fileToUpload"]["tmp_name"];
-
+    $errorMsg = "";
     $target_dir = "photo/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        // echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        $errorMsg = "This file is not an image.";
+        $uploadOk = 0;
     }
 
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        $errorMsg = "Sorry, this file already exists.";
         $uploadOk = 0;
     }
 
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        $errorMsg = "Sorry, your file is too large.";
         $uploadOk = 0;
     }
 
     // Allow certain file formats
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $errorMsg = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
-/*	
-    //validate file name including single qoute
-    if (preg_match("/'/u", $filename)) { 
-       echo "file name is not valid.";
-       $uploadOk = 0;
-    }
-*/
+
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+
+        echo "<font color='red'>" . $errorMsg . "</font>";
         // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($tempname, $target_file)) {
@@ -71,32 +77,56 @@ if (isset($_POST['upload']) && isset($_POST["about"]) && isset($_POST["title"]))
 
             sqlsrv_query($conn, $sql);
 
-            echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+            echo "<font color='green'>" . "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded." . "</font>";
+            echo " <br /> <br />";
+            echo "<a href=\"InsertIntoHome.php\" id=\"myBtn\" class=\"next\">Next &raquo;</a>";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            $errorMsg = "Sorry, there was an error uploading your file.";
         }
     }
 }
 ?>
 
-
-</head>
 <body>
-	<form method="POST" action="" enctype="multipart/form-data">
+
+	<form name="myForm" method="POST" enctype="multipart/form-data"
+		onchange="return filepreview()">
 		<h4>Upload an image for About page:</h4>
-		<input type="file" name="fileToUpload" value="" />
+		<input type="file" id="file" name="fileToUpload" value="  " />
+		<!-- Image preview -->
+		<br /> <br />
+		<div id="imagePreview"></div>
+
 		<h4>About page title:</h4>
 		<textarea rows="2" cols="50" type="text" name="title"></textarea>
-		<br />
-		<br />
+		<br /> <br />
 
 		<h4>About page content:</h4>
 		<textarea rows="22" cols="50" type="text" name="about"> </textarea>
 		<br /> <br />
 		<div>
-			<button type="submit" name="upload">Submit</button>
+			<button type="submit" name="submit">Submit</button>
 		</div>
+		<br /> <br />
 	</form>
+
+	<script> 
+	function filepreview() {
+	var fileInput = document.getElementById('file');
+				// Image preview 
+				if (fileInput.files && fileInput.files[0]) { 
+					var reader = new FileReader(); 
+					reader.onload = function(e) { 
+						document.getElementById( 
+							'imagePreview').innerHTML = 
+							'<img src="' + e.target.result 
+							+ '"/>'; 
+					}; 
+					
+					reader.readAsDataURL(fileInput.files[0]); 
+				} 
+	}
+	</script>
 
 </body>
 </html>
