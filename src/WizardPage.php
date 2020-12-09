3ba70edbeefa533d6150c89654b6f68612dd3ba7
dasb1996 <60516938+dasb1996@users.noreply.github.com>
2020-12-08 19:35:21 -0500

    fixes

--- src/WizardPage.php
+++ src/WizardPage.php
@@ -9,7 +9,7 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Admin Page</title>
+<title>Wizard Welcome Page</title>
 <link rel="stylesheet"
 	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
